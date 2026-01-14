2e0a529975ccc4bfa6ff9420a611bd59095efc19
ORÇUN GÜLEŞ <expo7089@gmail.com>
2026-01-14 11:56:00 +0300

    HOMERAZOR

--- obj/Release/net8.0/SAT242516005.AssemblyInfo.cs
+++ obj/Release/net8.0/SAT242516005.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-SAT242516005-5e0b976b-3271-4d3e-94ac-7895b9e2dd70")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAT242516005")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de3f3ea10e399b3ecb792f42fe04c23a7a51b704")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAT242516005")]
